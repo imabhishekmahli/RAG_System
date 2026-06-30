7487fe9cf4a7b614b5afa5714c0fff539d7da7c0
imabhishekmahli <abhi8h3k@gmail.com>
2026-06-30 22:20:03 +0530

    package installed

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -1,1 +1,16 @@
-#
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+  print("main function")
+
+if __name__ == "__main__":
+  main()
